5fa802809f89f28c20c46546e10e5029874debb7
shakilmahmud14 <forallworks1414@gmail.com>
2024-01-05 20:53:29 +0600

    add

--- assist/js/scripts.js
+++ assist/js/scripts.js
@@ -5,13 +5,19 @@ jQuery(document).ready(function(){
     jQuery(this).addClass("active");
   })
   jQuery(".navbar ul li a").click(function(){
-    jQuery(".navbar ul li a span").toggleClass("active-rect");
+    jQuery("ul li span").addClass("active-rect");
   })
 
 
+  jQuery(".list-item ul li a").click(function(){
+      jQuery(".list-item ul li a").removeClass("active");
+      jQuery(this).addClass("active");
+    })
 
 
-
+  jQuery(".delete").click(function(){
+    jQuery(this).parent(".data-base").addClass("data-base-delete");
+  })
 
 
 
